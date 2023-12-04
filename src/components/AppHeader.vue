c128d9a4b4dfbc33cde4a9d31df3bc2ad4393778
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2023-12-04 15:31:57 +0100

    add: navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,39 @@
 <script>
 export default {
-    
+
 }
 </script>
 
 <template>
-    <div>
-        
-    </div>
+        <nav class="navbar">
+            <div class="container justify-content-start gap-3 fs-6">
+                <span class="navbar-text text-white">
+                    Everything about Lifestyle, Travel and Gadgets! 
+                </span>
+                <span class="navbar-text text-white">
+                    <a href="">SIGN IN</a>
+                </span>
+                <span class="navbar-text text-white">
+                    <a href="">ABOUT US</a>
+                </span>
+                <span class="navbar-text text-white">
+                    <a href="">CONTACT US</a>
+                </span>
+                <span class="navbar-text text-white">
+                    <a href="">BUY NOW</a>
+                </span>
+            </div>
+        </nav>
 </template>
 
 <style lang="scss" scoped>
-    @use "../style/partials/variables" as *;
-    
+@use "../style/partials/variables" as *;
+
+nav {
+    background-color: $primary-color;
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+}
 </style>
