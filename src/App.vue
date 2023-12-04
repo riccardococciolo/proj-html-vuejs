25a67fb4de0f79f111d019dc93203719f30e5e0a
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2023-12-04 13:15:32 +0100

    add: project scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+import { store } from './store'
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+  components: { AppHeader, AppMain, AppFooter },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+    <AppHeader />
+  </header>
+  <main>
+    <AppMain />
+  </main>
+  <footer>
+    <AppFooter />
+  </footer>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use "./style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css";
 </style>
