9462c100fc6cfb6f69b9268eee6e65520ae20caa
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2023-12-05 12:55:07 +0100

    add: categories

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    props: {
+        text: String,
+    },
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="my-button py-2 px-5">
+        <span>{{ text }}</span>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../style/partials/variables" as *;
+
+.my-button {
+    background-color: $text-black;
+    color: white;
+}
+</style>
